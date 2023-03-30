c03d32f9624ed2c20da294a573c4c79263eaea36
GustavoMass <gmxirimba@gmail.com>
2023-03-30 19:39:34 -0300

    Iniciando em JS, variaveis e operadores

--- Meu projeto/script.js
+++ Meu projeto/script.js
@@ -1,4 +1,13 @@
 // alert("Um alerta é este bloco chato de aviso aqui");
 
-console.log("Hello World");
+// console.log("Hello World");
+
+var primeiravar = "Xablau, malandro!";
+
+console.log(primeiravar);
+
+
+
+
+
 
