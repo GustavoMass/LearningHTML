e61e602355661e99a5ccd2987bd94e68630c9c42
GustavoMass <gmxirimba@gmail.com>
2023-04-04 19:23:11 -0300

    Operadores, comparadores, tipos de dados e condicionais 1

--- Meu projeto/script.js
+++ Meu projeto/script.js
@@ -6,6 +6,13 @@ var primeiravar = "Xablau, malandro!";
 
 console.log(primeiravar);
 
+// prompt("Qual o seu nome?");
+
+// var nomeprompt = prompt("Qual o seu nome?");
+// alert("Bom dia, " + nomeprompt);
+
+// var numeroprompt = parseFloat(prompt("Digite um número"));
+
 
 
 
